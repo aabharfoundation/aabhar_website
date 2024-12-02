8b0c93398f990bfe054df01d51b5d4b366b4b682
aabharfoundation <ajayharde1234@gmail.com>
2024-12-02 17:37:56 +0000

    Update index.js

--- index/index.js
+++ index/index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Toggle the mobile menu
     hamburger.addEventListener('click', function () {
         hamburger.classList.tonggle('active');
-        nav-links.classList.toggle('active');
+        nav-links.classList.toggle('open');
         
     });
 
