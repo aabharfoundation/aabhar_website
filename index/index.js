9e601b49721c030967bbd720830e58e208d1dba4
aabharfoundation <ajayharde1234@gmail.com>
2024-12-03 07:36:26 +0000

    Update index.js

--- index/index.js
+++ index/index.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector('.hamburger'); // Select the hamburger icon
-    const nav-links = document.querySelector('.nav-links'); // Select the navigation links container
+    const hamBurger = document.querySelector('.hamburger'); // Select the hamburger icon
+    const navList = document.querySelector('.nav-links'); // Select the navigation links container
     const navItems = document.querySelectorAll('.nav-links li a'); // Select all the navigation links
     
     // Toggle the mobile menu when the hamburger is clicked
     hamburger.addEventListener('click', function () {
-        nav-links.classList.toggle('active');
-        hamburger.classList.toggle('active'); // Toggle the 'active' class on the hamburger (for transforming to 'X')
+        hamBurger.classList.toggle('active');
+        navList.classList.toggle('active');
+         // Toggle the 'active' class on the hamburger (for transforming to 'X')
          // Toggle the 'active' class on nav-links (for sliding in the menu)
     });
 
