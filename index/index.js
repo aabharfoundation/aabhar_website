c8d0f949ed4dcbbd7ce957bdc018ce5c6fb2c793
aabharfoundation <ajayharde1234@gmail.com>
2025-10-03 22:38:44 +0530

    Update index.js

--- index/index.js
+++ index/index.js
@@ -75,11 +75,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (leftButton && rightButton && wrapper) {
             leftButton.addEventListener('click', () => {
-                wrapper.scrollLeft -= 300;
+                wrapper.scrollLeft -= 240;
             });
 
             rightButton.addEventListener('click', () => {
-                wrapper.scrollLeft += 300;
+                wrapper.scrollLeft += 240;
             });
         }
     }
